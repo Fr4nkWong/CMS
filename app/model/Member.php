cf88a5f31caccd7de2afd7f47f300e5282bb4719
frankwong <frankwong96@qq.com>
2020-11-03 22:04:13 +0800

    featrue: wzy演示项目搭建

--- app/model/Member.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace app\model;
-
-use think\model;
-
-class Member extends Model
-{
-    
-}
--- config/database.php
+++ config/database.php
@@ -19,17 +19,17 @@ return [
     'connections'     => [
         'mysql' => [
             // 数据库类型
-            'type'            => env('database.type', ''),
+            'type'            => env('database.type', 'mysql'),
             // 服务器地址
-            'hostname'        => env('database.hostname', ''),
+            'hostname'        => env('database.hostname', '127.0.0.1'),
             // 数据库名
             'database'        => env('database.database', ''),
             // 用户名
-            'username'        => env('database.username', ''),
+            'username'        => env('database.username', 'root'),
             // 密码
             'password'        => env('database.password', ''),
             // 端口
-            'hostport'        => env('database.hostport', ''),
+            'hostport'        => env('database.hostport', '3306'),
             // 数据库连接参数
             'params'          => [],
             // 数据库编码默认采用utf8
