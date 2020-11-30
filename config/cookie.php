713797c8f69d4e3e10bd715cbe4b2ff522923028
Fr4nkWong <565123258@qq.com>
2020-11-30 11:08:33 +0800

    featrue: 完善配置注释

--- config/cookie.php
+++ config/cookie.php
@@ -5,14 +5,20 @@
 return [
     // cookie 保存时间
     'expire'    => 0,
-    // cookie 保存路径
+    // cookie 保存路径 只有此路径下的页面可读cookie
     'path'      => '/',
-    // cookie 有效域名 cookie由domain及其子domain共享
+    // cookie 有效域名 不允许跨域访问cookie，父域名cookie可由子域名使用，反之不行。
     'domain'    => '',
-    //  cookie 启用安全传输 是否走https
+    // cookie 启用安全传输 是否走https
     'secure'    => false,
-    // httponly设置 
+    // httponly设置 只有在http请求头中会带cookie，而不能通过document.cookie来访问此cookie
     'httponly'  => false,
-    // 是否使用 setcookie
-    'setcookie' => true,
 ];
+
+// domain
+// 为可以访问此cookie的域名，顶级域名只能设置domain为顶级域名，不能设置为二级域名或者三级域名，否则cookie无法生成。
+// 二级域名能读取设置了domain为顶级域名或者自身的cookie，不能读取其他二级域名domain的cookie。所以要想cookie在多
+// 个二级域名中共享，需要设置domain为顶级域名，这样就可以在所有二级域名里面或者到这个cookie的值了。
+
+    // 是否使用 setcookie
+    // 'setcookie' => true,
--- config/session.php
+++ config/session.php
@@ -8,9 +8,9 @@ return [
     'name'           => 'WZYSESSID',
     // SESSION_ID的提交变量,解决flash上传跨域
     'var_session_id' => '',
-    // 驱动方式 支持file cache  redis需要在cache.php中配置
+    // 驱动方式 支持file cache redis需要在cache.php中配置
     'type'           => 'file',
-    // 存储连接标识 当type设置为cache类型的时候指定存储标识 redis
+    // 存储连接标识 当type设置为cache类型的时候指定存储标识
     'store'          => null,
     // 过期时间
     'expire'         => 996,
@@ -19,3 +19,14 @@ return [
     // 序列化
     // 'serialize'     => ['json_encode', 'json_decode'],
 ];
+
+// DIY session driver
+// think\contract\SessionHandlerInterface
+// interface SessionHandlerInterface
+// {    // 在调用Session::start()的时候执行，并且只会执行一次
+//     public function read(string $sessionId): string; 
+//      // 在本地化会话数据的时候执行（调用Session::save()方法），系统会在每次请求结束的时候自动执行
+//     public function delete(string $sessionId): bool;
+//      // 在销毁会话的时候执行（调用Session::destroy()方法）
+//     public function write(string $sessionId, string $data): bool;
+// }
